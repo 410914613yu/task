5daaa26d7208ffa8d70250a596fc2d1f20a216f5
410914613yu <1352555372@qq.com>
2020-01-16 19:35:21 +0800

    “saunfazuoye

---
+++ suanfa002.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+ 
+int n,k,count=0;          //全局变量
+int a[21];
+int isprime(int num){
+    int i;
+    for(i=2;i*i<=num;i++)
+          if(num%i==0)
+          return 0;
+    return 1;
+}
+//p标记当前元素的下标，left 标记还有几个数没选出来
+void dfs(int sum,int p,int left){     
+    //因为n-1是数组最大的下标，所以left为0时，某一种组合已经完成
+    if(left==0){    
+	if(isprime(sum)){
+           count++;
+           return;
+        }
+    }
+     //n-left是当前所选下标的最大值，若超过，则会选不够；因为这是按照从前往后的顺序以此进行选择
+}
+int main(){
+   int i;
+   scanf("%d %d",&n,&k);
+   for(i=0;i<n;i++)
+   scanf("%d",a+i);
+   dfs(0,0,k);
+   printf("%d\n",count);
+   return 0;
+}
+ 
---
+++ suanfa003.c
@@ -0,0 +1,40 @@
+#include <stdio.h>  
+#include <string.h>  
+#include <stdlib.h>  
+int *out;  
+int out_size;  
+void bag(int n, int m)  
+{  
+        int i;  
+        if (n < 1 || m < 1 || (n == 1 && m != 1))  
+                return;  
+        if (m == n) {  
+                out[n] = 1;  
+                for (i = 1; i <= out_size; i++) {  
+                        if (out[i])  
+                                printf("%d ", i);  
+                }  
+                printf("\n");  
+                out[n] = 0;  
+        }  
+        bag(n - 1, m);  
+        out[n] = 1;  
+        bag(n - 1, m - n);  
+        out[n] = 0;  
+  
+}  
+  
+int main()  
+{  
+        int n, m;  
+        printf("Please input two num(n,m):");  
+        scanf("%d%d", &n, &m);  
+        if (n > m)  
+                n = m;  
+        out_size = n;  
+        out = (int *)malloc((n + 1) * sizeof(int));  
+        memset(out, 0, (n + 1) * sizeof(int));  
+        bag(n, m);  
+        free(out);  
+        return 0;  
+}  
